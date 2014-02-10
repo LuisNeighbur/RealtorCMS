cf0db52afa6282080ad2c63d0de65350a7666880
shaggikpo <shaggikpo@gmail.com>
2014-02-10 08:05:01 -0200

    Agregado espaciado a las rutas para mejorar comprension

--- app/routes.php
+++ app/routes.php
@@ -35,27 +35,45 @@ Route::group(array('before' =>'noGuest'), function(){
 	Route::get('/admin', function(){
 		return Redirect::to('/admin/add');
 	});
-	Route::get('/admin/add','PanelController@showAdd');
-	Route::get('/admin/del','PanelController@showDel');
-	Route::post('/admin/add','PanelController@add');
-	Route::post('/admin/del','PanelController@delete');
-	Route::post('/admin/edit','PanelController@patch');
+	Route::get('/admin/add', 'PanelController@showAdd');
+
+	Route::get('/admin/del', 'PanelController@showDel');
+
+	Route::post('/admin/add', array('before' => 'sanitize', 'uses'=> 'PanelController@add'));
+
+	Route::post('/admin/del', array('before' => 'sanitize', 'uses'=> 'PanelController@delete'));
+
+	Route::post('/admin/edit', array('before' => 'sanitize', 'uses'=> 'PanelController@patch'));
 	//Esta ruta post es temporal
-	Route::post('/admin/collector', array('before' => 'sanitize', 'uses'=>'CollectorController@getProperty'));
-	Route::post('/admin/upload', 'PanelController@uploadImage');
+	Route::post('/admin/collector', array('before' => 'sanitize', 'uses'=>' CollectorController@getProperty'));
+
+	Route::post('/admin/upload', array('before' => 'sanitize', 'uses'=> 'PanelController@uploadImage'));
 });
 Route::get('/','HomeController@showWelcome');
-Route::get('/getData/{id}.json', /*array('before' => 'sanitize', 'uses' => 'HomeController@showJson')*/'HomeController@showJson')
+
+Route::get('/getData/{id}.json', array('before' => 'sanitize', 'uses' => 'HomeController@showJson'))
 	->where(array('id'=>'[0-9]+'));
+
 Route::get('/knowme' , array('before' => 'sanitize', 'uses' => 'HomeController@showKnowMe'));
+
 Route::get('/buy' , array('before' => 'sanitize', 'uses' => 'HomeController@showBuy'));
+
 Route::get('/support' , array('before' => 'sanitize', 'uses' => 'HomeController@showSupport'));
+
 Route::get('/sell' , array('before' => 'sanitize', 'uses' => 'HomeController@showSell'));
+
 Route::get('/adv' , array('before' => 'sanitize', 'uses' => 'HomeController@showAdv'));
+
 Route::get('/center' , array('before' => 'sanitize', 'uses' => 'HomeController@showCenter'));
+
 Route::get('/contact' , array('before' => 'sanitize', 'uses' => 'HomeController@showContact'));
+
 Route::post('/contact/send' , array('before' => 'sanitize', 'uses' => 'HomeController@SendEMail'));
+
 Route::get('/{title}/{id}','HomeController@showProperty')->where(array('id' => '[0-9]+'));
+
 Route::get('/login', array('before' => 'guest','uses' =>'PanelController@login'));
+
 Route::post('/login', array('before' => 'csrf','uses' =>'PanelController@loginProcess'));
-Route::get('/search/{terms}' , 'SearchController@search');
+
+Route::get('/search' , 'SearchController@search');
